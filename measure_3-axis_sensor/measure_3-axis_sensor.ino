78e28886e1c585a3f8407bb1ac5cc9ab3fccea09
ATSU <mifutsua@gmail.com>
2024-02-02 15:15:17 +0900

    update 3-axis

--- measure_3-axis_sensor/measure_3-axis_sensor.ino
+++ measure_3-axis_sensor/measure_3-axis_sensor.ino
@@ -1,5 +1,6 @@
 #include <WioLTEforArduino.h>
 #include <ADXL345.h>
+#include <stdio.h>
 
 #define INTERVAL        (60000) // データ送信の間隔をミリ秒で指定
 #define RECEIVE_TIMEOUT (10000) // 受信タイムアウトをミリ秒で指定
@@ -10,33 +11,29 @@ ADXL345 Accel;
 void setup() {
   delay(200);
 
+  SerialUSB.begin(115200);
+  while (!SerialUSB); // For Native USB
   SerialUSB.println("");
   SerialUSB.println("--- START ---------------------------------------------------");
 
-  // Wio LTEモジュールの初期化
   SerialUSB.println("### I/O Initialize.");
   Wio.Init();
 
-  // 電源供給
   SerialUSB.println("### Power supply ON.");
   Wio.PowerSupplyGrove(true); // Groveコネクタの電源をON
   delay(500); // 電源が安定するまで待機
 
-  // 加速度センサの初期化
-  Accel.powerOn();
+  Accel.powerOn(); // 加速度センサの電源をON
 
-  // LTEモジュールの電源をON
   Wio.PowerSupplyLTE(true);
-  delay(500); // 電源が安定するまで待機
+  delay(500);
 
-  // LTEモジュールの起動またはリセット
   SerialUSB.println("### Turn on or reset.");
   if (!Wio.TurnOnOrReset()) {
     SerialUSB.println("### ERROR! ###");
     return;
   }
 
-  // Soracom Airに接続
   SerialUSB.println("### Connecting to \"soracom.io\".");
   if (!Wio.Activate("soracom.io", "sora", "sora")) {
     SerialUSB.println("### ERROR! ###");
@@ -47,40 +44,46 @@ void setup() {
 }
 
 void loop() {
+  char data[100];
   int x, y, z;
-  char data[256]; // JSONデータを格納するための配列
 
   // 加速度センサからのデータを読み取る
   Accel.readXYZ(&x, &y, &z);
-  // 加速度データとデバイスの稼働時間をJSON形式でフォーマット
-  sprintf(data, "{\"x\":%d,\"y\":%d,\"z\":%d,\"uptime\":%lu}", x, y, z, millis() / 1000);
+  sprintf(data, "{\"x\":%d,\"y\":%d,\"z\":%d}", x, y, z);
 
   SerialUSB.println("### Open.");
-  // Soracom Harvestにデータを送信するためのソケットを開く
   int connectId = Wio.SocketOpen("harvest.soracom.io", 8514, WIOLTE_UDP);
   if (connectId < 0) {
     SerialUSB.println("### ERROR! ###");
-    goto err;
+    // ここで直接遅延してからリターンする
+    delay(INTERVAL);
+    return; // エラー発生時に関数から抜ける
   }
 
   SerialUSB.println("### Send.");
   SerialUSB.print("Send: ");
   SerialUSB.println(data);
-  // ソケットを通じてデータを送信
   if (!Wio.SocketSend(connectId, data)) {
     SerialUSB.println("### ERROR! ###");
-    goto err_close;
+    Wio.SocketClose(connectId); // エラー発生時にソケットを閉じる
+    delay(INTERVAL);
+    return; // エラー発生時に関数から抜ける
   }
 
-  // 送信後、ソケットを閉じる
-  SerialUSB.println("### Close.");
-  if (!Wio.SocketClose(connectId)) {
+  SerialUSB.println("### Receive.");
+  int length = Wio.SocketReceive(connectId, data, sizeof(data), RECEIVE_TIMEOUT);
+  if (length < 0) {
     SerialUSB.println("### ERROR! ###");
-    // エラー処理
+  } else if (length == 0) {
+    SerialUSB.println("### RECEIVE TIMEOUT! ###");
+  } else {
+    SerialUSB.print("Receive: ");
+    SerialUSB.println(data);
   }
 
-err_close:
-  // エラーが発生した場合の処理
-err:
+  SerialUSB.println("### Close.");
+  Wio.SocketClose(connectId);
+
   delay(INTERVAL); // 次の送信までの間隔
 }
+
